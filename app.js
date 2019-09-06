6369cb3a3bae9d577050913e9848109b04556f3d
matheus de souza <matheussouza18@live.com>
2019-09-06 11:32:33 -0300

    Adicionado os metodos removeItem, getSinglebutton,clearCart, contudo após a execução a funcionalidade de add ao cart não funciona

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const cartItems = document.querySelector(".cart-items");
 const cartTotal = document.querySelector(".cart-total");
 const cartContent = document.querySelector(".cart-content");
 const productsDOM = document.querySelector(".products-center");
+const clearCart = document.querySelector(".clear-cart");
 
 // Cart
 let cart = [];
@@ -48,7 +49,7 @@ class UI {
            alt="product" class="product-img">
           <button class="bag-btn" data-id =${product.id}>
             <i class="fas fa-shopping-cart"></i>
-            add to bag
+            add to cart
           </button>
         </div>
         <h3>${product.title}</h3>
@@ -100,9 +101,9 @@ class UI {
     cartItems.innerText = itemsTotal;
   }
   addCartItem(item) {
-    const div = document.createElement("div");
-    div.classList.add("cart-item");
-    div.innerHTML = 
+    const jorge = document.createElement("div");
+    jorge.classList.add("cart-item");
+    jorge.innerHTML = 
     `<img src= ${item.image} alt="product">
     <div>
       <h4>${item.title}</h4>
@@ -114,7 +115,7 @@ class UI {
       <p class="item-amount" >${item.amount}</p>
       <i class="fas fa-chevron-down" data-id =${item.id}></i>
     </div>`;
-    cartContent.appendChild(div);
+    cartContent.appendChild(jorge);
   }
   showCart(){
     cartOverlay.classList.add('transparentBcg');
@@ -133,6 +134,35 @@ class UI {
   hideCart(){
     cartOverlay.classList.remove('transparentBcg');
     cartDOM.classList.remove('showCart');
+  }
+  cartLogic(){
+    //clear cart Button
+    clearCart.addEventListener('click',()=>{
+      this.clearCart();
+      
+    });
+  }
+  //cart Functionality
+  clearCart(){
+   let cartItems = cart.map(item => item.id);
+   cartItems.forEach(id => this.removeItem(id));
+   console.log(cartContent.children);
+   while(cartContent.children.length>0){
+     cartContent.removeChild(cartContent.children[0])
+   }
+   this.hideCart();
+  }
+  //
+  removeItem(id){
+    cart = cart.filter(item => item.id !== id);
+    this.setCartValues(cart);
+    Storage.saveCart(cart);
+    let button = this.getSingleButton(id);
+    button.dusabled = false;
+    button.innerHTML = `<i class= "fas fa-shopping-cart"><i> add to cart`;
+  }
+  getSingleButton(id){
+    return buttonsDOM.find(button => button.dataset.id === id);
   }
 }
 
@@ -168,5 +198,6 @@ document.addEventListener("DOMContentLoaded", () => {
     })
     .then(() => {
       ui.getBagButtons();
+      ui.cartLogic()
     });
 });
